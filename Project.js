b27dbb25185529a9fa260246b951981e0a8c6712
Katherine Williams <kawilliams8@gmail.com>
2019-08-29 15:24:46 -0600

    Inital styling

---
+++ Project.js
@@ -0,0 +1,25 @@
+class Project {
+  constructor(id, title, body, star, quality) {
+    this.id = id;
+    this.title = title || "";
+    this.body = body || "";
+    this.star = false;
+    this.quality = quality || 0;
+  }
+
+  saveToStorage() {
+
+  }
+
+  deleteFromStorage() {
+
+  }
+
+  updateIdea() {
+
+  }
+
+  updateQuality() {
+
+  }
+}
